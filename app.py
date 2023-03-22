3547d65547826c4af55a0d74197ea3f14b6c835c
peanutradio <114959220+peanutradio@users.noreply.github.com>
2023-03-22 15:39:04 +0900

    create python streamlit message

---
+++ app.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+from time import sleep
+
+# 페이지 기본 설치
+st.set_page_config(
+
+    page_icon="🛩️", 
+    page_title="Daniel의 스트림릿 배포하기",
+    layout="wide", 
+)
+
+# 페이지 헤더, 서브헤더 제목 설정
+st.header("Daniel 페이지에 오신 것을 환영합니다.👋")
+st.subheader("스트림 기능 맛보기")
+
+# 페이지 컬럼 분할(예: 부트스트랩 컬럼, 그리드)
+cols = st.columns((1, 1, 2))
+cols[0].metric("10/11", "15 °C", "2")
+cols[0].metric("10/12", "17 °C", "2 °F")
+cols[0].metric("10/13", "15 °C", "2")
+cols[1].metric("10/14", "17 °C", "2 °F")
+cols[1].metric("10/15", "14 °C", "-3 °F")
+cols[1].metric("10/16", "13 °C", "-1 °F")
+
+
+# 라인 그래프 데이터 생성(with. Pandas)
+chart_data = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=['a', 'b', 'c'])
+
+# 컬럼 나머지 부분에 라인차트 생성
+cols[2].line_chart(chart_data)
